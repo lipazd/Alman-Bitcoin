223fcf26eeda8368fa6e9031d43ee9de451277fd
lipazd <lipazdahan@gmail.com>
2020-07-30 13:05:25 +0300

    first commit

---
+++ part2.js
@@ -0,0 +1,7 @@
+   //part 2 of the test
+   
+   var tagA = document.getElementsByTagName('a');
+    for (let i = 0; i < tagA.length; i++) {
+        if (tagA[i].href.includes("xxx")) 
+            tagA[i].href += "?test=true"
+    }
