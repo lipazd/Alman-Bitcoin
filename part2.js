af31b305a2ecd0523d0a6aae364e78e2b50d2005
lipazd <lipazdahan@gmail.com>
2020-07-30 13:14:03 +0300

    commit3

--- part2.js
+++ part2.js
@@ -1,7 +1,8 @@
-   //part 2 of the test
-   
-   var tagA = document.getElementsByTagName('a');
-    for (let i = 0; i < tagA.length; i++) {
-        if (tagA[i].href.includes("xxx")) 
-            tagA[i].href += "?test=true"
-    }
+//part 2 of the test
+
+var tagA = document.getElementsByTagName('a');
+for (let i = 0; i < tagA.length; i++) {
+    if (tagA[i].href.includes("xxx"))
+        tagA[i].href += "?test=true"
+} 
+
